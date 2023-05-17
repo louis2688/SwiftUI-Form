f5f8e5a4767167ecf3e3ca046261eb652c9259e2
louis2688 <louismadrigal26@gmail.com>
2023-05-17 09:16:32 +0800

    Initial Commit

---
+++ SwiftUI-Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI-Form
+//
+//  Created by dev on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
